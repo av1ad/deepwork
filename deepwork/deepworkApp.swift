788ef5acd128d8f0840674dad3affb724d24c24b
av1ad <aviadchuraman@gmail.com>
2025-03-07 14:51:21 -0500

    Initial Commit

---
+++ deepwork/deepworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  deepworkApp.swift
+//  deepwork
+//
+//  Created by Aviad Churaman on 3/7/25.
+//
+
+import SwiftUI
+
+@main
+struct deepworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ deepworkTests/deepworkTests.swift
@@ -0,0 +1,16 @@
+//
+//  deepworkTests.swift
+//  deepworkTests
+//
+//  Created by Aviad Churaman on 3/7/25.
+//
+
+import Testing
+
+struct deepworkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
