0a2f1b2779d162def31cf12e9c61ed1c2f3e1882
av1ad <aviadchuraman@gmail.com>
2025-03-30 16:54:02 -0400

    Added background color and color scheme

--- deepwork/ContentView.swift
+++ deepwork/ContentView.swift
@@ -5,22 +5,27 @@
 //  Created by Aviad Churaman on 3/7/25.
 //
 
+// Color scheme:
+// https://coolors.co/332e3c-2176ae-57b8ff-cdd1c4-f4f9e9
+
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Color.gray.opacity(0.3)
+        Color(red: 51/255, green: 46/255, blue: 60/255)
             .overlay(
-        VStack {
-            Image(systemName: "book")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Deepwork")
-        }
-        .padding()
-        )
+                VStack {
+                    Image(systemName: "book")
+                        .imageScale(.large)
+                        .foregroundStyle(.tint)
+                    Text("Deepwork")
+                    Text("Your personal productivity assistant")
+                }
+                    .padding()
+            )
     }
 }
+
 
 #Preview {
     ContentView()
---
+++ deepwork/Preview Content/Timer.swift
@@ -0,0 +1,8 @@
+//
+//  Timer.swift
+//  deepwork
+//
+//  Created by Aviad Churaman on 3/30/25.
+//
+
+import SwiftUI
